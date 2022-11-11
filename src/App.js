c6e064c57387b68763c6c74fca682d8d7806609f
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2022-11-11 16:39:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Name from './components/Name';
+import Exper from './components/Exper';
+import Skills from './components/Skills';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='main'>
+      <Name/>
+      <Exper/>
+      <Skills/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Name.js
@@ -0,0 +1,12 @@
+const Name=()=>{
+    return(
+        <>
+        <h1>Bittoo Aggrawal</h1>
+        <p>software Developler, Educator Geekster</p>
+        <a href=''>Linkedin</a>
+      <a href=''>Github</a>
+        <hr/>
+        </>
+    )
+}
+export default Name;
