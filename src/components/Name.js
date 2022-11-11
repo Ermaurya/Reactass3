68d945742034aec93966b0e1b3b049294b3d2e0b
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2022-11-11 17:03:51 +0530

    Update Name.js

--- src/components/Name.js
+++ src/components/Name.js
@@ -3,8 +3,8 @@ const Name=()=>{
         <>
         <h1>Bittoo Aggrawal</h1>
         <p>software Developler, Educator Geekster</p>
-        <a href=''>Linkedin</a>
-      <a href=''>Github</a>
+        <a href='https://www.linkedin.com/in/shubham-maurya-56b613157/'>Linkedin</a>
+      <a href='https://github.com/Ermaurya/Reactass3'>Github</a>
         <hr/>
         </>
     )
